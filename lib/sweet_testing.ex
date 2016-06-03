143fd259db7159e81ca8ed2265cf9fe19229ba62
jamesv33 <james@verran.ca>
2016-06-03 15:24:55 -0700

    slightly more succinct

--- lib/sweet_testing.ex
+++ lib/sweet_testing.ex
@@ -21,55 +21,23 @@ defmodule S do
     """
   end
   def run do
-    query_node = request_body |> xpath(~x"//Query/cars"l)
-    get_brand(query_node)
-    # n = query_node |> get_node_names
-    # n2 = n |> List.first
-    # request_body |> xpath(~x"name(.)")
-    # request_body |> xpath(~x"//Query/#{n2}"k)
+    base_node = request_body |> xpath(~x"//Query/cars"l)
+    paths = ["bmw", "specs"]
+    process_node(base_node, paths)
+  end
+  def process_node(node, [ path | tail ]) do
+    node
+    |> Enum.map(fn (node) ->
+      child_node = node |> xpath(~x"./#{path}"l)
+      { node |> xpath(~x"name(.)"s) |> String.to_atom,
+        process_node(child_node, tail)
+      }
+    end)
+  end
+  def process_node(node, []) do
+    [ specs: node |> spec_details |> List.flatten ]
   end
 
-  def get_node_names(outer_node) do
-    outer_node
-    |> Enum.map(fn (node) ->
-      node |> xpath(~x"name(.)"s)
-    end)
-  end
-################
-  def get_cars_node(query_node) do
-    query_node
-    |> Enum.map(fn (node) ->
-      node_name = node |> xpath(~x"name(.)"l)
-      node_name
-    end)
-  end
-  ###########################
-  def get_cars(cars_node) do
-    brand_node = request_body |> xpath(~x"//Query/cars"l)
-    cars_node
-    |> Enum.map(fn (cars) ->
-      [ cars |> xpath(~x"name(.)"s),
-        get_brand(brand_node)
-      ]
-    end)
-  end
-  def get_brand(brand_node) do
-    specs_node = request_body |> xpath(~x"//Query/cars/bmw"l)
-    brand_node
-    |> Enum.map(fn (cars) ->
-      { cars |> xpath(~x"name(.)"s) |> String.to_atom,
-        get_specs(specs_node)
-      }
-    end)
-  end
-  def get_specs(brand_node) do
-    brand_node
-    |> Enum.map(fn (brand) ->
-      { brand |> xpath(~x"name(.)"s) |> String.to_atom,
-        [specs: brand |> xpath(~x"./specs"l) |> spec_details |> List.flatten]
-      }
-    end)
-  end
   def spec_details(spec_info) do
     spec_info
     |> Enum.map(fn (spec) ->
@@ -79,7 +47,4 @@ defmodule S do
       ]
     end)
   end
-
-
-
 end
