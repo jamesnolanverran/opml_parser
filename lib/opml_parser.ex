2fbe1a244d89c1134d3eb184ba6875499bbd1256
jamesv33 <james@verran.ca>
2016-06-02 17:18:40 -0700

    allow for arbitrary nesting

--- lib/opml_parser.ex
+++ lib/opml_parser.ex
@@ -3,14 +3,22 @@ defmodule OPMLParser do
 
   def run(file) do
     node = File.stream!(file) |> xpath(~x"//body/outline"l)
-    if node |> List.first |> xpath(~x"./outline") == nil do
-      parse_opml(node)
-    else
-      parse_opml(node, :nested)
-    end
+    parse_opml(node)
   end
 
   defp parse_opml(node) do
+    if node |> hasChildren? do
+      parse_node(node, :nested)
+    else
+      parse_node(node)
+    end
+  end
+
+  defp hasChildren?(node) do
+    node |> List.first |> xpath(~x"./outline") != nil
+  end
+
+  defp parse_node(node) do
     node
     |> Enum.map(fn (outline_node) ->
         %{
@@ -22,14 +30,14 @@ defmodule OPMLParser do
           }
     end)
   end
-  defp parse_opml(node, :nested) do
+  defp parse_node(node, :nested) do
     node
     |> Enum.map(fn (outline_node) ->
         %{
             text:       outline_node |> xpath(~x"./@text"s),
             title:      outline_node |> xpath(~x"./@title"s),
             children:   outline_node |> xpath(~x"./outline"l) |> parse_opml
-        }
+          }
     end)
   end
 end
