c9e9ab9038ab4e363c0f69f152213284f243c097
jamesv33 <james@verran.ca>
2016-06-04 15:21:04 -0700

    __using__ macro working - now to clean up __MODULE__

--- lib/sweet_testing.ex
+++ lib/sweet_testing.ex
@@ -1,24 +1,38 @@
 defmodule S do
-# defmodule SweetXmlTraversal do
+# defmodule S weetXmlTraversal do
+  defmacro __using__(_) do
+    quote do
+      # Define implementation for user modules to use
+      import SweetXml
+      import S
+      def handle_node(_, _), do: []
+
+      # Defoverridable makes the given functions in the current module overridable
+      # Without defoverridable, new definitions of greet will not be picked up
+      defoverridable [handle_node: 2]
+    end
+  end
   import SweetXml
-  defp traverse_nodes(outer_node, [ path | tail ]) do
+  def traverse_nodes(outer_node, [ path | tail ], x) do
     node_name = outer_node |> get_keyword_name
     outer_node
     |> Enum.map(fn (node) ->
       inner_node = node |> xpath(~x"./#{path}"l)
-      handle_node({node_name, inner_node}, tail)
+      x.handle_node({node_name, inner_node}, tail)
     end)
   end
-  defp traverse_nodes(outer_node, []) do # no more specified paths - leafy
+  def traverse_nodes(outer_node, [], x) do # no more specified paths - leafy
     node_name = outer_node |> get_keyword_name
-    handle_node({node_name, outer_node}, [])
+    X.handle_node({node_name, outer_node}, [])
   end
 
-  defp get_keyword_name(node) do
+  def get_keyword_name(node) do
     node |> List.first |> xpath(~x"name(.)"s) |> String.to_atom
   end
-
+end
   #================================
+defmodule X do
+  use S
   def run do
   # def run(filename, path) do
     doc = request_body # doc = File.stream!(filename // "dir/to/page.xml")
@@ -26,37 +40,32 @@ defmodule S do
                                   # that will be handled with handle_node
     [ base | paths ] = String.split(path, "/")
     base_node = doc |> xpath(~x"//#{base}"l) # paths need to include a surrounding element -> eg "body"
-    traverse_nodes(base_node, paths)
+    traverse_nodes(base_node, paths, __MODULE__)
   end
 
   # Use callbacks to handle specific nodes using :path
-  # defp handle_node({:specs, node}, []) do # final traversal for :spec branch
-  #   node
-  #   |> Enum.map(fn (specs_node) ->
-  #     {
-  #       :specs,
-  #       [
-  #         model: specs_node |> xpath(~x"./model/text()"s),
-  #         engine: specs_node |> xpath(~x"./engine/text()"s),
-  #         doesntexist: specs_node |> xpath(~x"./nonexistent/text()"s)
-  #       ]
-  #     }
-  #   end)
-  # end
-
-  # defp handle_node({node_name, node}, paths) do # for all cases but ":specs" in the path I build a nested keyword list
-  #   {
-  #     node_name,
-  #     traverse_nodes(node, paths)
-  #   }
-  # end
-  defp handle_node(_, []), do: [] # default on last node returns empty list
-  defp handle_node({_, node}, paths) do # default on all other nodes continue down path -
-    traverse_nodes(node, paths)
+  def handle_node({:specs, node}, []) do # final traversal for :spec branch
+    node
+    |> Enum.map(fn (specs_node) ->
+      {
+        :specs,
+        [
+          model: specs_node |> xpath(~x"./model/text()"s),
+          engine: specs_node |> xpath(~x"./engine/text()"s),
+          doesntexist: specs_node |> xpath(~x"./nonexistent/text()"s)
+        ]
+      }
+    end)
   end
-  defp handle_node(_, _), do: [] # default on last node returns empty list
 
-  defp request_body do
+  def handle_node({node_name, node}, paths) do # for all cases but ":specs" in the path I build a nested keyword list
+    {
+      node_name,
+      traverse_nodes(node, paths, __MODULE__)
+    }
+  end
+
+  def request_body do
     """
     <Query>
       <cars>
