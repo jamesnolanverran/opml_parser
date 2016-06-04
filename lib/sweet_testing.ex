61dad64a4111a7df8f17476e57ed52ad9d6438f4
jamesv33 <james@verran.ca>
2016-06-04 14:11:55 -0700

    callbacks working for sweetxmlTraversal, about to look into behaviours

--- lib/sweet_testing.ex
+++ lib/sweet_testing.ex
@@ -1,39 +1,62 @@
 defmodule S do
+# defmodule SweetXmlTraversal do
   import SweetXml
-  def run(base_node, path) do
-    paths = String.split(path, "/")
-    process_node(base_node, paths)
-  end
-  def process_node(outer_node, [ path | tail ]) do
+  defp traverse_nodes(outer_node, [ path | tail ]) do
+    node_name = outer_node |> get_keyword_name
     outer_node
     |> Enum.map(fn (node) ->
       inner_node = node |> xpath(~x"./#{path}"l)
-      node_name = node |> get_keyword_name
-      {
-        node_name,
-        callback({node_name, inner_node}, tail)
-      }
+      handle_node({node_name, inner_node}, tail)
     end)
   end
-  def process_node(outer_node, []) do
-    outer_node
-    |> Enum.map(fn (node) ->
-      node_name = node |> get_keyword_name
-      {
-        node_name,
-        outer_node
-      } |> callback([]) #indicates an outer leaf - combine both process_node functions? they are different
-    end)
-    |> List.flatten
+  defp traverse_nodes(outer_node, []) do # no more specified paths - leafy
+    node_name = outer_node |> get_keyword_name
+    handle_node({node_name, outer_node}, [])
   end
+
   defp get_keyword_name(node) do
-    node |> xpath(~x"name(.)"s) |> String.to_atom
+    node |> List.first |> xpath(~x"name(.)"s) |> String.to_atom
   end
-
-
 
   #================================
-  def request_body do
+  def run do
+  # def run(filename, path) do
+    doc = request_body # doc = File.stream!(filename // "dir/to/page.xml")
+    path = "Query/cars/bmw/specs" # travers these paths - all as lists - and a callback will fire for each node
+                                  # that will be handled with handle_node
+    [ base | paths ] = String.split(path, "/")
+    base_node = doc |> xpath(~x"//#{base}"l) # paths need to include a surrounding element -> eg "body"
+    traverse_nodes(base_node, paths)
+  end
+
+  # Use callbacks to handle specific nodes using :path
+  # defp handle_node({:specs, node}, []) do # final traversal for :spec branch
+  #   node
+  #   |> Enum.map(fn (specs_node) ->
+  #     {
+  #       :specs,
+  #       [
+  #         model: specs_node |> xpath(~x"./model/text()"s),
+  #         engine: specs_node |> xpath(~x"./engine/text()"s),
+  #         doesntexist: specs_node |> xpath(~x"./nonexistent/text()"s)
+  #       ]
+  #     }
+  #   end)
+  # end
+
+  # defp handle_node({node_name, node}, paths) do # for all cases but ":specs" in the path I build a nested keyword list
+  #   {
+  #     node_name,
+  #     traverse_nodes(node, paths)
+  #   }
+  # end
+  defp handle_node(_, []), do: [] # default on last node returns empty list
+  defp handle_node({_, node}, paths) do # default on all other nodes continue down path -
+    traverse_nodes(node, paths)
+  end
+  defp handle_node(_, _), do: [] # default on last node returns empty list
+
+  defp request_body do
     """
     <Query>
       <cars>
@@ -53,38 +76,4 @@ defmodule S do
     </Query>
     """
   end
-  def run2 do
-    base_node = request_body |> xpath(~x"//Query"l)
-    path = "cars/bmw/specs"
-    S.run(base_node, path)
-  end
-
-
-  def callback({:specs, node}, []) do # an outer leaf
-    node
-    |> Enum.map(fn (specs_node) ->
-      node_name = specs_node |> get_keyword_name
-      {
-        node_name,
-        [
-          model: specs_node |> xpath(~x"./model/text()"s),
-          engine: specs_node |> xpath(~x"./engine/text()"s),
-          doesntexist: specs_node |> xpath(~x"./nonexistent/text()"s)
-        ]
-      }
-    end)
-  end
-
-# do something with nodes --- I will have to generalize the default behaviour so it can be structured explicitly
-# no more keyword lists ---
-
-
-  def callback({_, node}, paths) do
-    S.process_node(node, paths)
-  end
-
-  def callback(_, _) do
-    []
-  end
-
 end
